4b2d7d19d1467c0c63d7d7706ea3276ceed32f53
shreyanshsingh <shreyanshsinghjee@gmail.com>
2020-10-14 17:59:29 +0530

    Updates

---
+++ precache-manifest.22d0a07f53721c875e98a710ff6f1bfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d211977e100ad5d876a94e74b13a8c90",
+    "url": "/shreyanshsing/reduxProject/index.html"
+  },
+  {
+    "revision": "212da2ca866a117db74c",
+    "url": "/shreyanshsing/reduxProject/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "9af45fa63af504b18370",
+    "url": "/shreyanshsing/reduxProject/static/js/2.0386c484.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shreyanshsing/reduxProject/static/js/2.0386c484.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "212da2ca866a117db74c",
+    "url": "/shreyanshsing/reduxProject/static/js/main.0a81be70.chunk.js"
+  },
+  {
+    "revision": "0e11b92f5b2b8ddc91b1",
+    "url": "/shreyanshsing/reduxProject/static/js/runtime-main.5c801350.js"
+  }
+]);
